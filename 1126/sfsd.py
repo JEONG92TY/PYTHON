221ef2e22a7b223cd5ace334e78bd93c17b98d5d
JEONG92TY <xoduddlrn@gmail.com>
2024-11-26 22:07:53 +0900

    1126 update

---
+++ 1126/sfsd.py
@@ -0,0 +1,6 @@
+list = [1,2,2,3,3,3]
+set = set(list)
+
+i = int(input())
+if i in list :
+    print("정답")
--- 1126/weather.py
+++ 1126/weather.py
@@ -6,16 +6,29 @@ weather_data = [
     ["2024-11-22", "서울", 8.3, 0.0],
     ["2024-11-22", "부산", 12.0, 0.0]
 ]
+'''
+def city_list() :
+    list = []
+    for i in range (len(weather_data)) :
+        init = weather_data[i][1]
+        list.append(init)
+        i =+ 1
+    return list
+'''
 
 def ave_tem(city) :
-    list = []
-    for i in range(len(weather_data)) :
-        if weather_data[i][1] == city :
-            list.append(weather_data[i][2])
-            i += 1
-        else :
-            i += 1       
-    print(f"{city}의 평균 온도는 {sum(list)/len(list):.2f}°C 입니다.")
+ #   if city in city_list() :
+        list = []
+        for i in range(len(weather_data)) :
+            if weather_data[i][1] == city :
+                list.append(weather_data[i][2])
+                i += 1
+            else :
+                i += 1       
+        print(f"{city}의 평균 온도는 {sum(list)/len(list):.2f}°C 입니다.")
+        print("")
+#    else :
+#        print("없는도시")
 
 def tem_mm(city) :
     list = []
@@ -26,6 +39,7 @@ def tem_mm(city) :
         else :
             i += 1       
     print(f"{city}의 최고 기온 : {max(list)}°C, 최저 기온 : {min(list)}°C")
+    print("")
 
 def rainfall(city) :
     list1 = []
@@ -38,6 +52,7 @@ def rainfall(city) :
     day = list(filter(lambda x : x > 0, list1))
     print(f"{city}의 총 강수량 : {sum(list1)}mm")
     print(f"{city}의 강수량이 있었던 날 : {len(day)}")
+    print("")
 
 def add() :
     d = input("날짜를 입력하세요 (YYYY-MM-DD) : ")
@@ -47,10 +62,14 @@ def add() :
     new = [d, c, t, r]
     weather_data.append(new)
     print(f"{c}의 날씨 데이터가 추가되었습니다.")
+    print("")
 
 def data_list() :
-    for data in weather_data :
-        print()
+    i = 0
+    for i in range(0, len(weather_data)) :
+        print(f"날짜 : {weather_data[i][0]}, 도시 : {weather_data[i][1]}, 기온 : {weather_data[i][2]}, 강수량 : {weather_data[i][3]}")
+        i += 1
+    print("")
 
 
 
@@ -63,36 +82,51 @@ def data_list() :
 
 
 
-
-
-
-print("[날씨 데이터 분석 프로그램]""\n"
+print("\n"
+      "\n"
+      "[날씨 데이터 분석 프로그램]""\n"
       "1. 평균 기온 계산""\n"
       "2. 최고/최저 기온 찾기""\n"
       "3. 강수량 분석""\n"
       "4. 날씨 데이터 추가""\n"
       "5. 전체 데이터 출력""\n"
-      "6. 종료""\n"
+      "6. 종료 --- 정태영 ""\n"
+      "\n"
       "")
 
-num = int(input("원하는 기능의 번호를 입력하세요 : "))
+while True :
 
-if num == 1 :
-    city = input("도시 이름을 입력하세요 : ")
-    ave_tem(city)
-elif num == 2 :
-    city = input("도시 이름을 입력하세요 : ")
-    tem_mm(city)
-elif num == 3 :
-    city = input("도시 이름을 입력하세요 : ")
-    rainfall(city)
-elif num == 4 :
-    add()
-elif num == 5 :
-    data_list()
-elif num == 6 :
-    print("종료합니다.")
-    #break >> while 추가 후
-else :
-    print("잘못된 번호입니다.")
-    #continue >> while 추가 후
+ #   def city_list() :
+ #       list = []
+ #       for i in range (len(weather_data)) :
+ #           init = weather_data[i][1]
+ #           list.append(init)
+ #           i =+ 1
+ #       return list
+
+    num = input("원하는 기능의 번호를 입력하세요 : ")
+    if num == "1" :
+        print("")
+        city = input("도시 이름을 입력하세요 : ")
+        ave_tem(city)
+    elif num == "2" :
+        print("")
+        city = input("도시 이름을 입력하세요 : ")
+        tem_mm(city)
+    elif num == "3" :
+        print("")
+        city = input("도시 이름을 입력하세요 : ")
+        rainfall(city)
+    elif num == "4" :
+        print("")
+        add()
+    elif num == "5" :
+        print("")
+        data_list()
+    elif num == "6" :
+        print("종료합니다.")
+        break
+    else :
+        print("잘못된 번호입니다.")
+        print("")
+        continue
